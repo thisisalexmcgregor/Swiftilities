41a78cac3561aac13307b930bd14673d14603719
William McGinty <mcgintw@gmail.com>
2016-11-13 23:26:34 -0600

    Add a shuffling utility

---
+++ Swiftilities/Classes/Collection+Shuffle.swift
@@ -0,0 +1,34 @@
+//
+//  Collection+Shuffle.swift
+//  MuziuQ
+//
+//  Created by William McGinty on 7/28/15.
+//  Copyright © 2015 William McGinty. All rights reserved.
+//
+
+import Foundation
+
+public extension MutableCollection where Index == Int {
+    
+    /// Shuffle the elements of `self` in-place
+    public mutating func shuffle() {
+        guard count >= 2 else { return }
+        
+        for i in startIndex ..< endIndex - 1 {
+            let j = (endIndex - i).random + i
+            if i != j {
+                swap(&self[i], &self[j])
+            }
+        }
+    }
+}
+
+public extension Collection {
+    
+    /// Return a copy of `self` with its elements shuffled
+    public func shuffled() -> [Iterator.Element] {
+        var list = Array(self)
+        list.shuffle()
+        return list
+    }
+}
